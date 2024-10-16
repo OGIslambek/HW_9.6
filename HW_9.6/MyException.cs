6b80c0a211e6e8cd0f1e20a0422815395b50acbd
OGislambek <ogislambek@gmail.com>
2024-10-16 22:04:21 +0300

    Добавьте файлы проекта.

---
+++ HW_9.6/MyException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_9._6
+{
+    internal class MyException
+    {
+        Exception[] exceptions = new Exception[5]
+        {
+            new NewException(""),
+            new DivideByZeroException("Знаменатель в операции деления или целого числа равен нулю."),
+            new ArgumentNullException("Аргумент, передаваемый в метод — null."),
+            new ArgumentOutOfRangeException("Аргумент находится за пределами диапазона допустимых значений."),
+            new IndexOutOfRangeException("Индекс находится за пределами границ массива или коллекции.")
+    };
+
+    }
+}
